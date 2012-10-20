345cdc315059aabafd0179949bfd1dbd26742cb9
syslogic <martin@codefx.biz>
2012-10-20 06:47:58 +0200

    drop-in description extended.

--- pagoda/wp-content/plugins/pagoda.php
+++ pagoda/wp-content/plugins/pagoda.php
@@ -1,17 +1,16 @@
 <?php
 /**
- * @package Filesytem Access
- * @version 1.0
+ * @package Filesytem Access Fix
+ * @version 1.0.1
  */
 /*
-Plugin Name: PagodaBox Filesystem Access Fix
+Plugin Name: Filesystem Access Fix
 Plugin URI: http://codefx.biz/
-Description: Overrides the default filesystem access in order to enable updates.
+Description: Overrides the default filesystem access method in order to enable update capabilities for themes & plugins on PagodaBox/Heroku.
 Author: Martin Zeitler
-Version: 1.0
+Version: 1.0.1
 Author URI: hhttp://codefx.biz/
 */
-
 if(is_admin() && current_user_can('manage_options')) {
 	add_filter('filesystem_method', create_function('$a', 'return "direct";' ));
 	define( 'FS_CHMOD_DIR', 0751 );
